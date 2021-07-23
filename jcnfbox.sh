b9ca0265d5d9858a89bd02bd662e56f63498f647
Netflixxp <61532011+Netflixxp@users.noreply.github.com>
2021-07-23 14:26:04 +0800

    Update jcnfbox.sh

--- jcnfbox.sh
+++ jcnfbox.sh
@@ -51,7 +51,7 @@ wget -O "/root/speedtest" "https://raw.githubusercontent.com/Netflixxp/jcnf-box/
 chmod +x "/root/speedtest"
 chmod 777 "/root/speedtest"
 yellow "下载完成,之后可执行 bash /root/speedtest 再次运行"
-bash "/root/speedtest"
+./speedtest
 }
 
 #获取本机IP
